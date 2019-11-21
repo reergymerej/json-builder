774aa03875e3fc572d42b76e6ac3cec11d165a06
Jeremy Greer <jex.grizzle@gmail.com>
2019-11-21 07:58:03 -0800

    Shorter type string

--- src/components/App.js
+++ src/components/App.js
@@ -2,48 +2,53 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { increment, decrement } from '../actions'
 
-const Value = ({type, text}) => (
+const Type = ({text}) => (
+  <div className="text-xs text-teal-400">
+    {text}
+  </div>
+)
+
+const ValueType = ({type, value}) => (
   <div className="flex items-center">
     <div className="mr-2">
-      {text}
-      </div>
-    <div className="text-xs text-teal-400">
-      {type}
+      {/* <Type text={type} /> */}
+      {value}
     </div>
   </div>
 )
 
 const BooleanComp = ({value}) => (
-  <Value
+  <ValueType
     type="boolean"
-    text={value ? 'true' : 'false'}
+    value={value ? 'true' : 'false'}
   />
 )
 
 const NullComp = () => (
-  <Value
+  <ValueType
     type="null"
-    text="null"
+    value="null"
   />
 )
 
 const NumberComp = ({value}) => (
-  <Value
+  <ValueType
     type="number"
-    text={value.toString()}
+    value={value.toString()}
   />
 )
 
 const StringComp = ({value}) => (
-  <Value
+  <ValueType
     type="string"
-    text={value}
+    value={value}
   />
 )
 
-const getType = (value) => {
-  const s = Object.prototype.toString.apply(value)
-  switch (s) {
+const getTypeString = (value) => Object.prototype.toString.apply(value)
+
+const getValueTypeComp = (typeString) => {
+  switch (typeString) {
     case '[object Boolean]':
       return BooleanComp
     case '[object Null]':
@@ -57,76 +62,102 @@ const getType = (value) => {
     case '[object Object]':
       return ObjectComp
     default:
-      throw new Error(`uknown type: ${s}`)
+      throw new Error(`uknown type: ${typeString}`)
   }
 }
 
-const NameValue = ({name, value}) => {
-  const ValueType = getType(value)
+const wrapPathNode = (node) => {
+  return `['${node}']`
+}
+
+const appendPath = (path, node) => {
+  return `${path}${wrapPathNode(node)}`
+}
+
+const NameValue = ({name, value, path}) => {
+  const typeString = getTypeString(value)
+  const shortTypeString = typeString
+    .replace(/\[.+ /, '')
+    .replace(/\]/, '')
+  const ValueTypeComp = getValueTypeComp(typeString)
+  const thisPath = path
+    ? appendPath(path, name)
+    : name
   return (
     <div className="border border-1 flex mb-1 p-1">
-      <div className="text-gray-600 mr-2">
+      <div className="text-gray-600 pr-2 mr-2 border-r text-right">
         {name}
+        <Type text={shortTypeString} />
       </div>
-      <ValueType
+      <ValueTypeComp
         value={value}
+        path={thisPath}
       />
     </div>
   )
 }
 
-const ArrayComp = ({value}) => (
+const ContainerComp = ({type, children}) => (
   <div className="ml-4">
-    {value.map((item, i) => {
-      return (
-        <NameValue
-          key={i}
-          name={i.toString()}
-          value={item}
-        />
-      )
-    })}
+    <ValueType
+      type={type}
+      value={children}
+    />
   </div>
 )
 
-
-const ObjectComp = ({value}) => (
-  <div className="ml-4">
-    {Object.keys(value).map((key) => {
-      const v = value[key]
-      return (
-        <NameValue
-          key={key}
-          name={key}
-          value={v}
-        />
-      )
-    })}
-  </div>
+const ArrayComp = ({value, path}) => (
+  <ContainerComp type="array">
+    {value.map((item, i) => (
+      <NameValue
+        key={i}
+        name={i.toString()}
+        value={item}
+        path={path}
+      />
+    ))}
+  </ContainerComp>
 )
+
+const ObjectComp = ({value, path}) => (
+  <ContainerComp type="object">
+    {Object.keys(value).map((key) => (
+      <NameValue
+        key={key}
+        name={key}
+        value={value[key]}
+        path={path}
+      />
+    ))}
+  </ContainerComp>
+)
+
+const sunnyJimbo = [
+  false,
+  true,
+  null,
+  'hello',
+  888,
+  [ 'a', 'b', 'c'],
+  {
+    foo: 'bar',
+    baz: null,
+    horses: [
+      { color: 'green', name: 'old greeny' },
+      { color: 'yellow', name: 'Jaund' },
+    ],
+  },
+]
+
+global.sunnyJimbo = sunnyJimbo
 
 class App extends Component {
   render() {
     return (
       <div className="container mx-auto my-8">
         <NameValue
-          name="sunny jimbo"
-          value={[
-            false,
-            true,
-            null,
-            'hello',
-            888,
-            [ 'a', 'b', 'c'],
-            {
-              foo: 'bar',
-              baz: null,
-              horses: [
-                { color: 'green', name: 'old greeny' },
-                { color: 'yellow', name: 'Jaund' },
-              ],
-            },
-          ]}
+          name="sunnyJimbo"
+          value={sunnyJimbo}
         />
       </div>
     )
